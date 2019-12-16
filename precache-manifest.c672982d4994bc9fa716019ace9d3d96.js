c236ee9e675786e54c0bbdd6735b36462eb71d69
Igor Ozheredov <o.igorok@gmail.com>
2019-12-16 07:44:34 +0200

    Updates

---
+++ precache-manifest.c672982d4994bc9fa716019ace9d3d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39c66b5d8c732b85c96f29b08d4c96b",
+    "url": "/KuYalda/react-hw4-2/index.html"
+  },
+  {
+    "revision": "6b0784c85ca61ff0a686",
+    "url": "/KuYalda/react-hw4-2/static/css/main.c7ab62b3.chunk.css"
+  },
+  {
+    "revision": "7c8c1b2062491d3db7e7",
+    "url": "/KuYalda/react-hw4-2/static/js/2.34a28090.chunk.js"
+  },
+  {
+    "revision": "6b0784c85ca61ff0a686",
+    "url": "/KuYalda/react-hw4-2/static/js/main.0dc4ea72.chunk.js"
+  },
+  {
+    "revision": "581f9470ac4f51e58fb2",
+    "url": "/KuYalda/react-hw4-2/static/js/runtime-main.a24c5153.js"
+  }
+]);
